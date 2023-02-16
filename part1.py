3c452f03c1af3f409fc8519273657a4fc6c29f12
peterthoang <hoangpeter7@gmail.com>
2023-02-16 16:19:47 -0600

    read input part1.py

--- part1.py
+++ part1.py
@@ -1,2 +1,11 @@
+import os
 import pandas as pd
 import numpy as np
+
+def read_data(input_file):
+    input_path = os.path.join(os.getcwd(), input_file)
+    df = pd.read_csv(input_path, header=None, sep='\s+', names=['seq_name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class'])
+    print(df.to_markdown())
+
+if __name__ == '__main__':
+    read_data('ecoli.data')
