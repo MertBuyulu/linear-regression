842b6e2dd39f9da43616fbc2cac998160528b280
Mert Buyulu <62453255+MertBuyulu@users.noreply.github.com>
2023-02-16 16:39:01 -0600

    Convert class data to numerical values

--- part1.py
+++ part1.py
@@ -5,7 +5,9 @@ import numpy as np
 def read_data(input_file):
     input_path = os.path.join(os.getcwd(), input_file)
     df = pd.read_csv(input_path, header=None, sep='\s+', names=['seq_name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class'])
+    df['class'].replace(['cp', 'im', "imS", "imL", "imU", "om", "omL", "pp"], [1, 2, 3, 4, 5, 6, 7, 8], inplace=True)
     print(df.to_markdown())
+
 
 if __name__ == '__main__':
     read_data('ecoli.data')
